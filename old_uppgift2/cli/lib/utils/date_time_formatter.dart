361ab7acf34f56b0f06b1ebaa0084141db894a2b
Dan Erlandsson <daningpublic@gmail.com>
2024-11-01 09:31:54 +0100

    Removed old_uppgift2 and uppgift2

--- old_uppgift2/cli/lib/utils/date_time_formatter.dart
+++
@@ -1,9 +0,0 @@
-// lib/utils/date_time_formatter.dart
-
-import 'package:intl/intl.dart';
-
-class DateTimeFormatter {
-  static String formatDate(DateTime date) {
-    return DateFormat('yyyy-MM-dd HH:mm').format(date);
-  }
-}
