b7133e5aaf04df9d575d7808c7d5d9eb3b273267
Dan Erlandsson <daningpublic@gmail.com>
2024-11-03 19:46:50 +0100

    removing old dart_application_1

--- dart_application_1/bin/dart_application_1.dart
+++
@@ -1,5 +0,0 @@
-import 'package:dart_application_1/menu.dart';
-
-void main(List<String> arguments) {
-  menu();
-}
--- dart_application_1/lib/models/parking_space.dart
+++
@@ -1,7 +0,0 @@
-class ParkingSpace {
-  final String id;
-  final String address;
-  int pricePerHour;
-
-  ParkingSpace(this.id, this.address, this.pricePerHour);
-}
