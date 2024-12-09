fe882cbb38e9439893f4c00e482ac6aa3f878c26
Dan Erlandsson <daningpublic@gmail.com>
2024-12-09 14:49:02 +0100

    main.dart recreated

--- uppgift3/parking_app/lib/main.dart
+++ uppgift3/parking_app/lib/main.dart
@@ -1,179 +1,61 @@
-// ignore_for_file: use_build_context_synchronously
-
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import 'package:client_repositories/async_http_repos.dart';
-import 'package:shared/shared.dart';
-import 'package:parking_app/auth_service.dart';
+import 'package:parking_app/views/user_page.dart';
+import 'package:parking_app/views/vehicles_page.dart';
 import 'package:provider/provider.dart';
+import 'package:parking_app/auth_service.dart';
+import 'package:parking_app/views/login_view.dart';
+import 'package:go_router/go_router.dart';
+import 'views/home_page.dart';
+import 'views/parking_page.dart';
+import 'views/parking_spaces_page.dart';
 
-class ParkingView extends StatefulWidget {
-  const ParkingView({super.key});
-
-  @override
-  State<ParkingView> createState() => _ParkingViewState();
+void main() {
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => AuthService(),
+      child: ParkingApp(),
+    ),
+  );
 }
 
-class _ParkingViewState extends State<ParkingView> {
-  late Future<List<Parking>> ongoingParkings;
-  late Future<List<Parking>> allParkings;
-
-  @override
-  void initState() {
-    super.initState();
-    loadParkings();
-  }
-
-  void loadParkings() {
-    final currentUser = context.read<AuthService>().username;
-    ongoingParkings = ParkingRepository().getAll().then((parkings) => parkings
-        .where((p) =>
-            p.vehicle.target?.owner.target?.name == currentUser &&
-            p.endTime == null)
-        .toList());
-    allParkings = ParkingRepository().getAll().then((parkings) => parkings
-        .where((p) => p.vehicle.target?.owner.target?.name == currentUser)
-        .toList());
-  }
-
+class ParkingApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Parking'),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.refresh),
-            onPressed: () {
-              setState(() {
-                loadParkings();
-              });
-            },
-          ),
-        ],
-      ),
-      body: FutureBuilder<List<Parking>>(
-        future: ongoingParkings,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          }
-
-          if (snapshot.hasError) {
-            return Center(child: Text('Error: ${snapshot.error}'));
-          }
-
-          if (!snapshot.hasData || snapshot.data!.isEmpty) {
-            return const Center(
-                child: Text('No ongoing parking records found.'));
-          }
-
-          final parkings = snapshot.data!;
-          return ListView.builder(
-            itemCount: parkings.length,
-            itemBuilder: (context, index) {
-              final parking = parkings[index];
-              return ListTile(
-                title: Text(
-                  'Parking at ${parking.parkingSpace.target?.address ?? 'Unknown'}',
-                ),
-                subtitle: Text(
-                  'Vehicle: ${parking.vehicle.target?.licensePlate ?? 'Unknown'}\n'
-                  'Start Time: ${parking.startTime}',
-                ),
-                trailing: ElevatedButton(
-                  onPressed: () async {
-                    await ParkingRepository().stop(parking.id);
-                    setState(() {
-                      loadParkings();
-                    });
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(content: Text('Parking stopped')),
-                    );
-                  },
-                  child: const Text('Stop'),
-                ),
-              );
-            },
-          );
-        },
-      ),
-      floatingActionButton: Row(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          FloatingActionButton(
-            heroTag: 'home',
-            onPressed: () {
-              context.go('/'); // Navigate to the home page
-            },
-            child: const Icon(Icons.home),
-          ),
-          const SizedBox(width: 16),
-          FloatingActionButton.extended(
-            heroTag: 'history',
-            onPressed: () async {
-              await showDialog(
-                context: context,
-                builder: (context) {
-                  return AlertDialog(
-                    title: const Text('Parking History'),
-                    content: FutureBuilder<List<Parking>>(
-                      future: allParkings,
-                      builder: (context, snapshot) {
-                        if (snapshot.connectionState ==
-                            ConnectionState.waiting) {
-                          return const Center(
-                              child: CircularProgressIndicator());
-                        }
-
-                        if (snapshot.hasError) {
-                          return Text('Error: ${snapshot.error}');
-                        }
-
-                        if (!snapshot.hasData || snapshot.data!.isEmpty) {
-                          return const Text('No parking history found.');
-                        }
-
-                        final parkings = snapshot.data!;
-                        return SizedBox(
-                          width: double.maxFinite,
-                          child: ListView.builder(
-                            shrinkWrap: true,
-                            itemCount: parkings.length,
-                            itemBuilder: (context, index) {
-                              final parking = parkings[index];
-                              return ListTile(
-                                title: Text(
-                                  'Parking at ${parking.parkingSpace.target?.address ?? 'Unknown'}',
-                                ),
-                                subtitle: Text(
-                                  'Vehicle: ${parking.vehicle.target?.licensePlate ?? 'Unknown'}\n'
-                                  'Start Time: ${parking.startTime}\n'
-                                  'End Time: ${parking.endTime ?? 'Ongoing'}',
-                                ),
-                              );
-                            },
-                          ),
-                        );
-                      },
-                    ),
-                    actions: [
-                      TextButton(
-                        onPressed: () {
-                          Navigator.of(context).pop();
-                        },
-                        child: const Text('Close'),
-                      ),
-                    ],
-                  );
-                },
-              );
-            },
-            label: const Text('History'),
-            icon: const Icon(Icons.history),
-          ),
-        ],
-      ),
+    return MaterialApp.router(
+      routerConfig: _router,
+      debugShowCheckedModeBanner: false,
     );
   }
+
+  final GoRouter _router = GoRouter(
+    initialLocation: '/login', // Start with the login page
+    routes: [
+      GoRoute(
+        path: '/login',
+        builder: (context, state) => const LoginView(),
+      ),
+      GoRoute(
+        path: '/',
+        builder: (context, state) => const HomePage(),
+      ),
+      GoRoute(
+        path: '/my-vehicles',
+        builder: (context, state) => const VehiclesView(),
+      ),
+      GoRoute(
+        path: '/parking',
+        builder: (context, state) => const ParkingView(),
+      ),
+      GoRoute(
+        path: '/parking-spaces',
+        builder: (context, state) => const ParkingSpacesView(),
+      ),
+      GoRoute(
+        path: '/user-page',
+        builder: (context, state) => const UserPage(),
+      ),
+    ],
+  );
+
+  ParkingApp({super.key});
 }
